a7f3701ccbbf7db9cf7258dfeb45db9d79979254
Ahmet Tufan Helvaci <athelvaci@gmail.com>
2016-05-06 20:50:44 +0000

    final website

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField
- 
-class ContactForm(Form):
-  name = TextField("Name")
-  email = TextField("Email")
-  subject = TextField("Subject")
-  message = TextAreaField("Message")
-  submit = SubmitField("Send")
