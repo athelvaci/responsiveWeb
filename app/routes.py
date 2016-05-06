c136299fbbfe0282b9d737c8b2cee422df733008
Ahmet Tufan Helvaci <athelvaci@gmail.com>
2016-05-06 17:29:56 +0000

    some changes in stylesheet and added comments div in index

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
- 
-app = Flask(__name__)      
- 
+import os
+from flask import Flask, render_template, request
+from forms import ContactForm
+
+app = Flask(__name__)
+
+app.secret_key = 'development key'
+
 @app.route('/')
 def home():
   return render_template('home.html')
- 
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  form = ContactForm()
+  
+  if request.method == 'POST':
+    return 'Form posted.'
+  
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
+  
 if __name__ == '__main__':
   app.run(debug=True)
+  
